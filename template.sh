72579842a77115e09afc8aadac39f963e4f1fc61
Realida <bipanapariyar04@gmail.com>
2026-05-03 11:14:35 +0545

    folder structure

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p resources
+
+touch src/init.py
+touch helper.py
+touch src/prompt.py
+touch requirements.txt
+touch README.md
+touch resources/trials.ipynb
+touch setup.py
+touch app.py
+
+echo "package installation completed"
